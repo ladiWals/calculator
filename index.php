0bbcd5f7b638631ab7a6e915f4ec395fd87d0d0a
ladiwals <vladjeslav@gmail.com>
2021-02-06 10:09:09 +0300

    добавил выбор действия и наладил основную логику работы

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <?php 
 // Основной блок исполнения логики
-$result = 0;
+if($_POST['submit']) {
+	switch($_POST['action']) {
+		case '+':
+			$result = $_POST['first'] + $_POST['second'];
+			break;
+		case '-':
+			$result = $_POST['first'] - $_POST['second'];
+			break;
+		case '*':
+			$result = $_POST['first'] * $_POST['second'];
+			break;
+		case '/':
+			$result = $_POST['first'] / $_POST['second'];
+			break;
+	}
+}
 ?>
 
 <!DOCTYPE html>
 <head>
+	<meta charset="utf-8">
 	<title>Калькулятор</title>
 	<link rel="SHORTCUT ICON" href="favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="styles.css">
@@ -12,14 +28,22 @@ $result = 0;
 
 <body>
 	<h1>Простейший калькулятор</h1>
+	<!-- <pre><?//php var_dump($_POST);?></pre> -->
 	<div class="buttons">
-		<form>
-			<input type="text" name="first" size="10">
-			<input type="text" name="second" size="10">
-			<input type="submit" value="=">
-			<span class="pseudoInput">
-				<label><?=$result?></label>
-			</span>
+		<form action="/" method="POST">
+			<div class="radioList">
+				<ul>
+					<li><input type="radio" name="action" value="+"> + (сложение)</li>
+					<li><input type="radio" name="action" value="-"> - (вычитание)</li>
+					<li><input type="radio" name="action" value="*"> * (умножение)</li>
+					<li><input type="radio" name="action" value="/"> / (деление)</li>
+				</ul>
+			</div>
+			<input type="text" name="first" size="10" value="<?=$_POST['first'] ?? ''?>">
+			<label><?=$_POST['action'] ?? '_'?></label>
+			<input type="text" name="second" size="10" value="<?=$_POST['second'] ?? ''?>">
+			<input type="submit" name = "submit" value="=">
+			<label class="pseudoInput"><?=$result?></label>
 		</form>
 	</div>
 </body>
